ed4959448b5619bc5e333b2753c0a22038047b48
neeksan <krysteffsm@gmail.com>
2023-07-04 12:56:41 +0300

    initial commit

--- src/main/java/training/day2/Hw3.java
+++ src/main/java/training/day2/Hw3.java
@@ -1,11 +1,12 @@
 package training.day2;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Hw3 {
     public static void main(String[] args) {
-        List<String> words = new ArrayList<String>();
+        List<String> words = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < 4; i++) {
@@ -14,7 +15,7 @@ public class Hw3 {
 
         words.removeIf(word -> word.length() < 4);
 
-        for (String word:words){
+        for (String word : words) {
             System.out.println(word);
         }
         scanner.close();
