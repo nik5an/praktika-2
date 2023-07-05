aa8c6e914a27a36450756ecd3c38edd25f9e870a
neeksan <krysteffsm@gmail.com>
2023-07-05 11:50:21 +0300

    initial commit

---
+++ src/main/java/training/day3/Hw1.java
@@ -0,0 +1,27 @@
+package training.day3;
+import java.util.Scanner;
+
+public class Hw1 {
+
+    static int min(int[] arr){
+        int min = arr[0];
+        for (int num:arr) {
+            if (min > num){
+                min = num;
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        for(int i = 0; i < 3; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println(min(arr));
+
+        scanner.close();
+    }
+}
