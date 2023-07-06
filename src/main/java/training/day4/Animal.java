9572288241338f62f2b897385720c4a5af41894e
neeksan <krysteffsm@gmail.com>
2023-07-06 10:09:44 +0300

    initial commit

---
+++ src/main/java/training/day4/Animal.java
@@ -0,0 +1,7 @@
+package training.day4;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ src/main/java/training/day4/Task1.java
@@ -0,0 +1,13 @@
+package training.day4;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        animal.eat();
+        dog.bark();
+        cat.meow();
+    }
+}
